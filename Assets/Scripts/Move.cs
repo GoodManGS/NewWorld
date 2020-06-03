29828601c1a6ffa2004f257d7ea3e982db378c05
crashdowngs <crashdowngs@gmail.com>
2020-06-03 19:24:57 +1000

    first

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateSize(float newSize)
+    {
+        
+        transform.localScale = new Vector3(Mathf.RoundToInt(newSize), 0, 0) + transform.localScale;
+    }
+}
